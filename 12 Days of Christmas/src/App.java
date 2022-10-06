729b39a867ad65531768d997a9d18b1fefd7015a
lrbianchi <113638363+lrbianchi@users.noreply.github.com>
2022-10-06 12:47:44 -0700

    Uploaded Files

---
+++ 12 Days of Christmas/src/App.java
@@ -0,0 +1,94 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Day1();
+        Partridge();
+        Day2();
+        TurtleDove();
+        Day3();
+        FrenchHen();
+        Day4();
+        CallingBirds();
+        Day5();
+
+        
+
+    
+    }
+    public static void TrueLove() {
+        System.out.println("my true love sent to me");
+    }
+
+    public static void Day1() {
+        System.out.println("On the first day of Christmas,");
+        TrueLove();
+    }
+    public static void Partridge() {
+        System.out.println("A partridge in a pear tree.");
+    }
+    public static void TurtleDove() {
+        System.out.println("two turtle doves, and");
+        Partridge();
+    }
+    public static void Day2() {
+        System.out.println("On the second day of Christmas,");
+        TrueLove();
+    }
+    public static void Day3() {
+        System.out.println("On the third day of Christmas,");
+        TrueLove();
+    }
+    public static void FrenchHen() {
+        System.out.println("Three French Hens,");
+        TurtleDove();
+    }
+    public static void Day4() {
+        System.out.println("On the fourth day of Christmas,");
+        TrueLove();
+    }
+    public static void CallingBirds() {
+        System.out.println("Four calling birds,");
+        FrenchHen();
+    }
+    public static void Day5() {
+        System.out.println("On the fifth day of Christmas,");
+        TrueLove();
+    }
+    public static void GoldRing() {
+        System.out.println("Five golden rings,");
+        CallingBirds();
+    }
+    public static void Day6() {
+        System.out.println("On the sixth day of Christmas,");
+        TrueLove();
+    }
+    public static void Geese() {
+        System.out.println("Six geese a-laying,");
+        GoldRing();
+    }
+    public static void Day7() {
+        System.out.println("On the seventh day of Christmas,");
+        TrueLove();
+    }
+    public static void Swans() {
+        System.out.println("Seven swans a-swimming,");
+        Geese();
+    }
+    public static void Day8() {
+        System.out.println("On the eighth day of Christmas,");
+        TrueLove();
+    }
+    public static void Maids() {
+        System.out.println("Eight maids a-milking,");
+        Swans();
+    }
+    public static void Day9() {
+        System.out.println("On the ninth day of Christmas,");
+        TrueLove();
+    }
+    public static void Ladies() {
+        System.out.println("Nine ladies dancing,");
+        Maids();
+    }
+
+
+}
